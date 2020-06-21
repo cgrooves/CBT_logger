153e6f397abd1ba1b8d55fb49100a0e1c76c8dbe
cgrooves <caleb.j.groves@gmail.com>
2020-06-20 21:33:15 -0700

    Make emotions list creation better
    Utilitze the example from w3schools on a todo list for the emotions
    list. Include the slider and a delete button for mistakes.

--- cbt_logger/static/emotions.js
+++ cbt_logger/static/emotions.js
@@ -1,17 +1,42 @@
-function addEmotion() {
-    // Get the list and click-able item
-    emotionsList = document.getElementById("emotions_list");
-    addItem = document.getElementById("add_emotions");
 
-    // add a new list item
-    emotion = document.createElement("li");
-    emotion.innerHTML = "another emotion";
-    emotion.addEventListener("dblclick", removeEmotion);
+function addEmotion() {
     
-    // append
-    emotionsList.insertBefore(emotion, addItem.nextSibling)
+    // Get the input value
+    let emotionText = document.getElementById("emotionsInput").nodeValue;
+    if (emotionText == "")
+    {
+        alert("You must enter text for the emotion!");
+        return;
+    }
+    document.getElementById("emotionsInput").nodeValue = "";
+    
+    // Create the new emotion item
+    let newEmotion = document.createElement("li");
+    newEmotion.appendChild(document.createTextNode(emotionText));
+
+    // Append the emotion
+    let emotionsList = document.getElementById("emotionsList");
+    emotionsList.appendChild(newEmotion);
+
+    // Create the Slider
+    let slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = 0;
+    slider.max = 100;
+    slider.value = 50;
+    slider.step = 5;
+    newEmotion.appendChild(slider);
+
+    // Create the "delete" button
+    let close = document.createElement("span");
+    let closeText = document.createTextNode("\u00D7");
+    close.className = "listClose";
+    close.onclick = removeEmotion;
+    close.appendChild(closeText);
+    newEmotion.appendChild(close);
 }
 
 function removeEmotion() {
-    this.parentNode.removeChild(this);
+    let listItem = this.parentElement;
+    emotionsList.removeChild(listItem);
 }
