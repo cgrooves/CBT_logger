81a373b48a0e1f8bc2f09375f35aee3c1742cb7a
cgrooves <caleb.j.groves@gmail.com>
2020-06-22 21:06:42 -0700

    Fix layout and interaction for emotions
    Fix the way the loading of the script is done (put in head instead of
    body). Also add some more CSS spacing for the emotions list elements,
    and remove the javascript from inside the HTML source.

--- cbt_logger/static/emotions.js
+++ cbt_logger/static/emotions.js
@@ -1,9 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Enter-Key event for text area
+    document.querySelector('#emotionsInput').onkeyup = (event) => {
+        if (event.keyCode == 13)
+        {
+            document.getElementById("emotionButton").click();
+        }
+    };
+    // onclick for button
+    document.querySelector('#emotionButton').onclick = addEmotion;
+});
 
 function addEmotion() {
     
     // Get the input value
     let emotionText = document.getElementById("emotionsInput").value;
-    console.log("adding emotion text " + emotionText);
     if (emotionText == "")
     {
         alert("You must enter text for the emotion!");
@@ -13,9 +23,10 @@ function addEmotion() {
     
     // Create the new emotion item
     let newEmotion = document.createElement("li");
+    newEmotion.className = "emotionItem";
     newEmotion.appendChild(document.createTextNode(emotionText));
 
-    // Append the emotion
+    // Append the emotion to the list
     let emotionsList = document.getElementById("emotionsList");
     emotionsList.appendChild(newEmotion);
 
@@ -26,6 +37,7 @@ function addEmotion() {
     slider.max = 100;
     slider.value = 50;
     slider.step = 5;
+    slider.className = "emotionSlider";
     newEmotion.appendChild(slider);
 
     // Create the "delete" button
