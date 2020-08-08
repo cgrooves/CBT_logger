11daf4f55bb14db6facaa1458e59850a51daca9e
cgrooves <caleb.j.groves@gmail.com>
2020-08-08 16:41:49 -0700

    Save emotion name in dataset

--- cbt_logger/static/emotions.js
+++ cbt_logger/static/emotions.js
@@ -6,17 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById("emotionButton").click();
         }
     };
-    // onclick for button
+    // onclick for buttons
     document.querySelector('#emotionButton').onclick = addEmotion;
     document.querySelector('#pushEmotionsBtn').onclick = pushEmotions;
 });
 
 function pushEmotions() {
-    let xhttp = new XMLHttpRequest();
-
-    // Open up POST request
-    xhttp.open('POST', '/emotions/push', true);
-
+    
     // Gather up the emotions
     let emotions = document.getElementsByClassName("emotionItem");
     if (emotions.length == 0)
@@ -24,21 +20,27 @@ function pushEmotions() {
         alert("No emotions specified! Please list at least one emotion.");
         return;
     }
-
+    
     // Create JSON object for HTML elements
     let out = [];
     for (let i = 0; i < emotions.length; i++)
     {
         let emotionJSON = {
-                "id": emotions[i].dataset.id,
-                // "range": emotions[i].querySelector("#emotionSlider").value,
-                "name": emotions[i].
+            "id": emotions[i].dataset.id,
+            // "range": emotions[i].querySelector("#emotionSlider").value,
+            "name": emotions[i].dataset.name
         };
-
+        
         // Add emotion JSON to list
         console.log("ID: " + emotionJSON.id + ", EMOTION: " + emotionJSON.name);
         out.push(emotionJSON);
     }
+
+    // Open up the AJAX cxn
+    let xhttp = new XMLHttpRequest();
+
+    // Open up POST request
+    xhttp.open('POST', '/emotions/push', true);
 
     // Send the JSON
     xhttp.send(JSON.stringify(out));
@@ -54,13 +56,18 @@ function addEmotion() {
         alert("You must enter text for the emotion!");
         return;
     }
+    // reset the input field string
     document.getElementById("emotionsInput").value = "";
     
     // Create the new emotion item
     let newEmotion = document.createElement("li");
     newEmotion.className = "emotionItem";
-    newEmotion.dataset.id = -1;
+    // group the text in with the list item
     newEmotion.appendChild(document.createTextNode(emotionText));
+
+    // Create the custom data
+    newEmotion.dataset.id = -1;
+    newEmotion.dataset.name = newEmotion.innerText || newEmotion.textContent;
 
     // Append the emotion to the list
     let emotionsList = document.getElementById("emotionsList");
