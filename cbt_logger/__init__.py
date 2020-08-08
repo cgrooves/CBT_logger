4e79fee5d1075ac66c1ddde6d33351b1121eddea
cgrooves <caleb.j.groves@gmail.com>
2020-08-07 20:58:20 -0700

    Create migration cmds

--- cbt_logger/__init__.py
+++ cbt_logger/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
+
 from .config import Config
 
 app = Flask(__name__)
@@ -9,7 +11,10 @@ app.config.from_object(Config)
 
 # Create database instance
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+
+# Login manager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
--- cbt_logger/config.py
+++ cbt_logger/config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'e57e70c34719a8f6d181fc5384d8cca6'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    
+    # Database config
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
